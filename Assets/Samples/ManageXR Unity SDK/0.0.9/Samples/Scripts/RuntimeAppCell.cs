3e644be85f9f01e021579f80f74483f7e6c4d667
Denis Babineau <dbabineau@transfrvr.com>
2023-01-13 16:30:53 -0400

    adding mxr sdk with samples

---
+++ Assets/Samples/ManageXR Unity SDK/0.0.9/Samples/Scripts/RuntimeAppCell.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+
+using UnityEngine.UI;
+
+namespace MXR.SDK.Samples {
+    public class RuntimeAppCell : MonoBehaviour {
+        public RuntimeApp runtimeApp;
+        public AppInstallStatus status;
+
+        [SerializeField] Text title;
+        [SerializeField] Image icon;
+        [SerializeField] Image updateIndicator;
+        [SerializeField] Image readyIndicator;
+        [SerializeField] Text statusLabel;
+        [SerializeField] Sprite defaultIcon;
+        [SerializeField] Image internetRequirement;
+        [SerializeField] Image controllerRequirement;
+
+        [ContextMenu("Refresh")]
+        public void Refresh() {
+            title.text = runtimeApp.title;
+
+            // Instead of MXRStorage.GetFullPath(video.iconPath) you can also use
+            // video.iconUrl to download the icon from a server.
+            ImageDownloader.New().Download(MXRStorage.GetFullPath(runtimeApp.iconPath),
+                x => {
+                    if (isBeingDestroyed) return;
+
+                    if (x == null) {
+                        icon.sprite = defaultIcon;
+                        return;
+                    }
+
+                    icon.sprite = Sprite.Create(x, new Rect(0, 0, x.width, x.height), Vector2.one / 2);
+                    icon.preserveAspect = true;
+                }
+            );
+
+            SetRequirementIcon(controllerRequirement, runtimeApp.controllersRequired);
+            SetRequirementIcon(internetRequirement, runtimeApp.internetRequired);            
+
+            if (status != null) {
+                if (status.IsNotUpdating()) {
+                    SetStatus(null);
+                    updateIndicator.enabled = false;
+                    var isComplete = status.status == AppInstallStatus.Status.COMPLETE;
+                    readyIndicator.enabled = false;
+                } else if (status.UpdateIsQueued()) {
+                    SetStatus("Queued...");
+                    updateIndicator.enabled = false;
+                    readyIndicator.enabled = false;
+                } else if (status.IsUpdating()) {
+                    switch (status.status) {
+                        case AppInstallStatus.Status.DOWNLOADING:
+                            SetStatus("Downloading..." + status.progress + "%");
+                            break;
+                        case AppInstallStatus.Status.PATCHING:
+                            SetStatus("Patching..." + status.progress + "%");
+                            break;
+                        case AppInstallStatus.Status.INSTALLING:
+                            SetStatus("Installing..." + status.progress + "%");
+                            break;
+                        case AppInstallStatus.Status.CLEANUP:
+                            SetStatus("Cleaning up...");
+                            break;
+                        case AppInstallStatus.Status.SETUP:
+                            SetStatus("Setup...");
+                            break;
+                        case AppInstallStatus.Status.READY_TO_INSTALL:
+                            SetStatus("Ready to install...");
+                            break;
+                    }
+                    updateIndicator.enabled = true;
+                    updateIndicator.fillAmount = status.progress / 100f;
+                    readyIndicator.enabled = false;
+                }
+            }
+        }
+
+        void SetRequirementIcon(Image icon, Content.Requirement requirement) {
+            switch (requirement) {
+                case Content.Requirement.UNDEFINED:
+                    icon.transform.parent.gameObject.SetActive(false);
+                    break;
+                case Content.Requirement.OPTIONAL:
+                    icon.transform.parent.gameObject.SetActive(true);
+                    icon.color = Color.yellow;
+                    break;
+                case Content.Requirement.MANDATORY:
+                    icon.transform.parent.gameObject.SetActive(true);
+                    icon.color = Color.green;
+                    break;
+            }
+        }
+
+        void SetStatus(string text) {
+            if (string.IsNullOrEmpty(text)) {
+                statusLabel.transform.parent.GetComponent<Image>().enabled = false;
+                statusLabel.text = "";
+            } else {
+                statusLabel.transform.parent.GetComponent<Image>().enabled = true;
+                statusLabel.text = text;
+            }
+        }
+
+        public void OnClick() {
+            Debug.Log("Open App " + runtimeApp.title);
+            if (Application.platform == RuntimePlatform.Android)
+                MXRAndroidUtils.LaunchRuntimeApp(runtimeApp);
+        }
+
+        bool isBeingDestroyed = false;
+        void OnDestroy() {
+            isBeingDestroyed = true;
+        }
+    }
+}
---
+++ Assets/Samples/ManageXR Unity SDK/0.0.9/Samples/Scripts/WebXRAppCell.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MXR.SDK.Samples {
+    public class WebXRAppCell : MonoBehaviour {
+        public WebXRApp webXRApp;
+        [SerializeField] Text title;
+        [SerializeField] Image icon;
+        [SerializeField] Sprite defaultIcon;
+        [SerializeField] Image internetRequirement;
+        [SerializeField] Image controllerRequirement;
+
+        [ContextMenu("Refresh")]
+        public void Refresh() {
+            title.text = webXRApp.title;
+
+            SetRequirementIcon(controllerRequirement, webXRApp.controllersRequired);
+            SetRequirementIcon(internetRequirement, webXRApp.internetRequired);
+
+            // Instead of MXRStorage.GetFullPath(video.iconPath) you can also use
+            // video.iconUrl to download the icon from a server.
+            ImageDownloader.New().Download(MXRStorage.GetFullPath(webXRApp.iconPath),
+                x => {
+                    if (isBeingDestroyed) return;
+
+                    if (x == null) {
+                        icon.sprite = defaultIcon;
+                        return;
+                    }
+
+                    icon.sprite = Sprite.Create(x, new Rect(0, 0, x.width, x.height), Vector2.one / 2);
+                    icon.preserveAspect = true;
+                }
+            );
+        }
+
+        void SetRequirementIcon(Image icon, Content.Requirement requirement) {
+            switch (requirement) {
+                case Content.Requirement.UNDEFINED:
+                    icon.transform.parent.gameObject.SetActive(false);
+                    break;
+                case Content.Requirement.OPTIONAL:
+                    icon.transform.parent.gameObject.SetActive(true);
+                    icon.color = Color.yellow;
+                    break;
+                case Content.Requirement.MANDATORY:
+                    icon.transform.parent.gameObject.SetActive(true);
+                    icon.color = Color.green;
+                    break;
+            }
+        }
+
+        public void OnClick() {
+            if (!string.IsNullOrEmpty(webXRApp.url)) {
+                Debug.Log("Open URL " + webXRApp.url);
+                Application.OpenURL(webXRApp.url);
+            }
+        }
+
+        bool isBeingDestroyed = false;
+        void OnDestroy() {
+            isBeingDestroyed = true;
+        }
+    }
+}
